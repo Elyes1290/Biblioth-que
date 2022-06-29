8378fc5f947b1be40e4b58b7b78c59db33853300
Elyes1290 <99724750+Elyes1290@users.noreply.github.com>
2022-06-29 16:50:09 +0200

    Initial project3

--- app/Http/Resources/AuthorResource.php
+++ app/Http/Resources/AuthorResource.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 use App\Http\Resources\CountryResource;
 
+use App\Models\Book;
+
 class AuthorResource extends JsonResource
 {
     /**
@@ -16,13 +18,19 @@ class AuthorResource extends JsonResource
      */
     public function toArray($request)
     {
+
+        $authors = $this->id;
         // return parent::toArray($request);
         return [
             'id' =>$this->id,
             'name' =>$this->name,
             'city' =>$this->city,
             'birthdate' =>$this->birthdate,
-            'country_id' =>$this->country_id
+            'country_id' =>$this->country_id,
+            'books' => Book::whereHas('authors', function($q) use ($authors) {
+                $q->where('author_id', $authors);
+            })->get('title')
+            
         ];
     }
 }
